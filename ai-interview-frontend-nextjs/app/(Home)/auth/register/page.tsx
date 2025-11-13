caba46238ef264367b01cd9d6ba84fe080340b4f
Pranit Daphale <pranitworkspace@gmail.com>
2025-11-13 19:46:03 +0530

    completed google auth

--- ai-interview-frontend-nextjs/app/(Home)/auth/register/page.tsx
+++ ai-interview-frontend-nextjs/app/(Home)/auth/register/page.tsx
@@ -60,6 +60,7 @@ export default function Register() {
   // 🔹 Handle Google Login
   const handleGoogleSuccess = async (credentialResponse: any) => {
     const credential = credentialResponse?.credential;
+    console.log(credential,"google")
     if (!credential) return toast.error("Google login failed!");
     const res: any = await dispatch(asyncGoogleLogin(credential));
     if (res.success) {
