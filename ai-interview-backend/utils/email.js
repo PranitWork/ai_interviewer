d838ebbe2e3e347f4bee931d00fcc965e5ccc6cf
Pranit Daphale <pranitworkspace@gmail.com>
2025-11-18 23:10:13 +0530

    code commited

--- ai-interview-backend/utils/email.js
+++ ai-interview-backend/utils/email.js
@@ -1,34 +1,13 @@
-import Brevo from "@getbrevo/brevo";
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(
-  Brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
-);
-
-const sendEmail = async (options) => {
-  try {
-    console.log("📨 Sending email to:", options.email);
-    console.log("🔑 Brevo API Key Exists:", !!process.env.BREVO_API_KEY);
-    console.log("📤 Sender:", process.env.BREVO_FROM);
-
-    const emailData = {
-      sender: {
-        name: "SwarAI",
-        email: process.env.BREVO_FROM,
-      },
-      to: [{ email: options.email }],
-      subject: options.subject,
-      htmlContent: options.message,
-    };
-
-    const result = await apiInstance.sendTransacEmail(emailData);
-    console.log("Email sent:", result);
-    return result;
-  } catch (error) {
-    console.error("❌ Brevo Error:", error.response?.body || error);
-    throw error;
-  }
+const sendEmail = async ({ email, subject, message }) => {
+  await resend.emails.send({
+    from: "SwarAi <onboarding@resend.dev>",
+    to: email,
+    subject,
+    html: message,
+  });
 };
 
 export default sendEmail;
